731763f2009b3625649a2b9647f43bef4b53f43d
Martin Johnson <M.J.Johnson@massey.ac.nz>
2021-05-01 10:34:47 +1200

    add webpage

---
+++ src/webpage.c
@@ -0,0 +1,20 @@
+char * main_page_html="<!DOCTYPE html> \
+<html><style>a {text-decoration: none;color:#000000;}  \
+div {padding:15px;} \
+</style> \
+<body style=\"font-family:Verdana;\"> \
+<a href=/red> \
+<div style=\"background-color:#f10101;\"> \
+  <h1>Red</h1> \
+</div> \
+<a href=/green> \
+<div style=\"background-color:#01f101;\"> \
+  <h1>Green</h1> \
+</div> \
+<a style=\"color:#ffffff;\" href=/blue> \
+<div style=\"background-color:#0101f1;\"> \
+  <h1>Blue</h1> \
+</div> \
+</body> \
+</html> \
+";
